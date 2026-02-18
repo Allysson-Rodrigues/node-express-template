1a467a52f7ae1ee3566c9d5acc22de56c01d4dec
Allysson Rodrigues <alissonrodriig@gmail.com>
2026-02-18 11:55:41 -0300

    chore: migrate eslint config to ESM and remove duplicated cjs config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,33 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    languageOptions: { 
-      globals: globals.node,
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      ".next/**",
+      "out/**",
+    ],
+  },
+  {
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      },
       sourceType: "module",
       ecmaVersion: "latest"
-    } 
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    }
   },
   pluginJs.configs.recommended,
+  prettierConfig,
 ];
